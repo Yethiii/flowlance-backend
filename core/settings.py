43a55069690426d0d35696c915e39cb01d7dd85b
LaetitiaV <laetitia.voue@gmail.com>
2026-03-13 10:41:00 +0100

    Accès Frontend

--- core/settings.py
+++ core/settings.py
@@ -117,9 +117,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 AUTH_USER_MODEL = 'users.User'
 
