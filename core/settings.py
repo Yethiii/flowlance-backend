c93bef13a067bd2164a8e281169668a06e1b5955
LaetitiaV <laetitia.voue@gmail.com>
2026-03-29 19:13:00 +0200

    Mise en ligne Render

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+    "https://flowlance-frontend.vercel.app/"]
 
 CORS_ALLOW_CREDENTIALS = True
 
